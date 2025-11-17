837860d23d501103d2c9a5b31d0463abe0042ebf
BlueBedMC <bluebed@ocebedwars.com>
2025-11-17 13:15:17 +1300

    strip type updated

--- src/main/java/com/bluebed/strip/StripType.java
+++ src/main/java/com/bluebed/strip/StripType.java
@@ -1,8 +1,20 @@
 package com.bluebed.strip;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public enum StripType {
-    GRASS,
-    ROAD,
-    RIVER,
-    TRAIN
+    GRASS(25, 20),
+    PARK(5, 70),
+    ROAD(25, 50),
+    MOTORWAY(5, 90),
+    RIVER(25, 60),
+    TRAIN(15, 30);
+
+    // Values out of 100
+    private final int spawnChance; // Must add to 100
+    private final int repetitionChance; // Not required to add to 100
 }
